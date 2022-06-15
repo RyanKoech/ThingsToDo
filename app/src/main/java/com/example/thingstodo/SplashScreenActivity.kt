adf8a37b45ac38b759bfac4a12aca274e83a23c4
RyanKoech <sirryankoech@gmail.com>
2022-06-15 13:31:33 +0300

    Implemented a splash screen

--- app/src/main/java/com/example/thingstodo/SplashScreenActivity.kt
+++ app/src/main/java/com/example/thingstodo/SplashScreenActivity.kt
@@ -1,11 +1,33 @@
 package com.example.thingstodo
 
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
 
 class SplashScreenActivity : AppCompatActivity() {
+
+    lateinit var handler: Handler
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){
+            window.setDecorFitsSystemWindows(false)
+        }else {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_VISIBLE
+        }
+
+        supportActionBar?.hide()
+        handler = Handler(Looper.getMainLooper())
+        handler.postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 1500)
+
     }
 }
