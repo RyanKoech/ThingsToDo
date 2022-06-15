9d98ed087e9629f4c5a71c808063954dc4173e5f
RyanKoech <sirryankoech@gmail.com>
2022-06-15 13:00:18 +0300

    Refactored SplashScreen filename to SplashScreenActivity

--- app/src/main/java/com/example/thingstodo/SplashScreenActivity.kt
+++ app/src/main/java/com/example/thingstodo/SplashScreenActivity.kt
@@ -3,7 +3,7 @@ package com.example.thingstodo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class SplashScreen : AppCompatActivity() {
+class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
