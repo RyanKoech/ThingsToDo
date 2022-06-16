209eb751877a840013bf40c2e272b01e0d3663e5
RyanKoech <sirryankoech@gmail.com>
2022-06-16 14:23:14 +0300

    Initialized Nav Controller and Logic

--- app/src/main/java/com/example/thingstodo/MainActivity.kt
+++ app/src/main/java/com/example/thingstodo/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.thingstodo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController : NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.main_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
