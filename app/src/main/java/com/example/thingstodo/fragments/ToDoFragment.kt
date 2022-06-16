6f916b6369f9007fcbfcc1d699e7cf65b70a7a57
RyanKoech <sirryankoech@gmail.com>
2022-06-16 14:24:02 +0300

    Added Logic to inflate ToDo Fragment

--- app/src/main/java/com/example/thingstodo/fragments/ToDoFragment.kt
+++ app/src/main/java/com/example/thingstodo/fragments/ToDoFragment.kt
@@ -1,5 +1,34 @@
 package com.example.thingstodo.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.thingstodo.databinding.FragmentToDoBinding
 
-class ToDoFragment : Fragment()
+class ToDoFragment : Fragment() {
+    private var _binding: FragmentToDoBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateView (
+        inflater : LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle? ) : View? {
+
+        _binding = FragmentToDoBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onDestroyView (){
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
