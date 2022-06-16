add4f0d8176e5df27615c2ecd8db9eb3e2e24ac4
RyanKoech <sirryankoech@gmail.com>
2022-06-16 16:16:51 +0300

    Added Data Class for the a to-do item

---
+++ app/src/main/java/com/example/thingstodo/model/ThingToDo.kt
@@ -0,0 +1,10 @@
+package com.example.thingstodo.model
+
+import java.time.LocalDateTime
+
+data class ThingToDo(
+    val id : Int,
+    val name: String,
+    val description: String,
+    val date: String
+)
