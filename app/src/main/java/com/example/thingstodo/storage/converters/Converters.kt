7cab92af6f39ad8394e4eaf2d9ddea15a08648a5
RyanKoech <sirryankoech@gmail.com>
2022-06-19 20:08:38 +0300

    Added Date<->Long type converter logic

--- app/src/main/java/com/example/thingstodo/storage/converters/Converters.kt
+++ app/src/main/java/com/example/thingstodo/storage/converters/Converters.kt
@@ -1,4 +1,16 @@
 package com.example.thingstodo.storage.converters
 
+import androidx.room.TypeConverter
+import java.util.*
+
 class Converters {
+    @TypeConverter
+    fun fromTimeStamp(value: Long?) : Date?{
+        return value?.let { Date(it) }
+    }
+
+    @TypeConverter
+    fun dateToTimeStamp(date: Date?) :Long?{
+        return date?.time
+    }
 }
