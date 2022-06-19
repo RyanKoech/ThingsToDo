20b0366468283184fee98acfc080cf03ddc7950d
RyanKoech <sirryankoech@gmail.com>
2022-06-19 20:41:35 +0300

    Implementated Database Class

--- app/src/main/java/com/example/thingstodo/storage/database/ThingToDoRoomDatabase.kt
+++ app/src/main/java/com/example/thingstodo/storage/database/ThingToDoRoomDatabase.kt
@@ -1,4 +1,33 @@
 package com.example.thingstodo.storage.database
 
-class ThingToDoRoomDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.thingstodo.storage.dao.ThingToDoDao
+import com.example.thingstodo.storage.model.ThingToDo
+
+@Database(entities = [ThingToDo::class], version = 1, exportSchema = false)
+abstract class ThingToDoRoomDatabase() :RoomDatabase() {
+
+    abstract fun thingToDoDao() : ThingToDoDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE : ThingToDoRoomDatabase? = null
+
+        fun getDatabase(context: Context) : ThingToDoRoomDatabase{
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    ThingToDoRoomDatabase::class.java,
+                    "thing_to_do_database"
+                )
+                    .fallbackToDestructiveMigration() //Data will be lost of there is a schema change
+                    .build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
 }
