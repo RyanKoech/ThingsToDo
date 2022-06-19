708cdfedf31cb3593c0db0c2623df775a2d3b290
RyanKoech <sirryankoech@gmail.com>
2022-06-19 20:24:18 +0300

    Defined Data Access Object methods

--- app/src/main/java/com/example/thingstodo/storage/dao/ThingToDo.kt
+++ app/src/main/java/com/example/thingstodo/storage/dao/ThingToDo.kt
@@ -1,4 +1,24 @@
 package com.example.thingstodo.storage.dao
 
+import androidx.room.*
+import com.example.thingstodo.storage.model.ThingToDo
+import kotlinx.coroutines.flow.Flow
+
+@Dao
 interface ThingToDo {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertThingToDo(thingToDo: ThingToDo)
+
+    @Update
+    suspend fun updateThingToDo(thingToDo: ThingToDo)
+
+    @Delete
+    suspend fun deleteThingToDo(thingToDo: ThingToDo)
+
+    @Query("SELECT * FROM thing_to_do WHERE id = :id")
+    fun getThingToDo(id: Int) : Flow<ThingToDo>
+
+    @Query("SELECT * FROM thing_to_do ORDER BY time_stamp ASC")
+    fun getThingsToDo() : Flow<List<ThingToDo>>
 }
