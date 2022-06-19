e5ac04d53281a0041397ecd95c9545e396fb17db
RyanKoech <sirryankoech@gmail.com>
2022-06-19 19:58:05 +0300

    Defined database table entity

--- app/src/main/java/com/example/thingstodo/storage/ThingToDo.kt
+++ app/src/main/java/com/example/thingstodo/storage/ThingToDo.kt
@@ -1,4 +1,16 @@
 package com.example.thingstodo.storage
 
-class ThingToDo {
-}
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.*
+
+@Entity(tableName = "thing_to_do")
+data class ThingToDo(
+    @PrimaryKey(autoGenerate = true)
+    val id : Int,
+    val name: String,
+    val description: String,
+    @ColumnInfo(name = "date_time")
+    val datetime: Date
+)
