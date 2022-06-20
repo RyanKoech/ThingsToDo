d56c6cf4e376d974da74ac6f57ab68be262ee7d3
RyanKoech <sirryankoech@gmail.com>
2022-06-20 14:30:02 +0300

    Implemented ViewModel Factory

--- app/src/main/java/com/example/thingstodo/viewmodel/ThingToDoViewModel.kt
+++ app/src/main/java/com/example/thingstodo/viewmodel/ThingToDoViewModel.kt
@@ -1,4 +1,19 @@
 package com.example.thingstodo.viewmodel
 
-class ThingToDoViewModel {
+import android.view.View
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.thingstodo.storage.dao.ThingToDoDao
+
+class ThingToDoViewModel(private val thingToDoDao: ThingToDoDao) :ViewModel() {}
+
+class ThingToDoViewModelFactor(private val thingToDoDao: ThingToDoDao) : ViewModelProvider.Factory {
+
+    override fun <T: ViewModel?> create (modelClass: Class<T>) : T{
+        if(modelClass.isAssignableFrom(ThingToDoViewModel::class.java)){
+            @Suppress("UNCHECKED_CAST")
+            return ThingToDoViewModel(thingToDoDao) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel Class")
+    }
 }
