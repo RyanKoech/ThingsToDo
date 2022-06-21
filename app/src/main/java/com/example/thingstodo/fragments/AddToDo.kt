57a3b0e53667ba5f7808902a0f17f46038469173
RyanKoech <sirryankoech@gmail.com>
2022-06-21 08:31:55 +0300

    Initialized viewmodel

--- app/src/main/java/com/example/thingstodo/fragments/AddToDo.kt
+++ app/src/main/java/com/example/thingstodo/fragments/AddToDo.kt
@@ -5,11 +5,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import com.example.thingstodo.databinding.FragmentAddToDoBinding
+import com.example.thingstodo.storage.application.ThingToDoApplication
+import com.example.thingstodo.viewmodel.ThingToDoViewModel
+import com.example.thingstodo.viewmodel.ThingToDoViewModelFactor
 
 class AddToDo : Fragment() {
     private var _binding : FragmentAddToDoBinding? = null
     private val binding get() = _binding!!
+
+    private val viewModel : ThingToDoViewModel by activityViewModels{
+        ThingToDoViewModelFactor(
+            (activity?.application as ThingToDoApplication).database
+                .thingToDoDao()
+        )
+    }
 
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
