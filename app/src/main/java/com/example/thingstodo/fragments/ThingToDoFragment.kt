f7ed4ffacfe80d6184e8202dbfa02312a9f30ce1
RyanKoech <sirryankoech@gmail.com>
2022-06-21 14:16:03 +0300

    Added new fragment files

---
+++ app/src/main/java/com/example/thingstodo/fragments/ThingToDoFragment.kt
@@ -0,0 +1,10 @@
+package com.example.thingstodo.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.thingstodo.R
+
+class ThingToDoFragment : Fragment()
