f922a90552d7b5c8e5e7b9d2df41e97a37e42ef1
RyanKoech <sirryankoech@gmail.com>
2022-06-21 08:30:36 +0300

    Populated ViewModel

--- app/src/main/java/com/example/thingstodo/viewmodel/ThingToDoViewModel.kt
+++ app/src/main/java/com/example/thingstodo/viewmodel/ThingToDoViewModel.kt
@@ -3,9 +3,35 @@ package com.example.thingstodo.viewmodel
 import android.view.View
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
 import com.example.thingstodo.storage.dao.ThingToDoDao
+import com.example.thingstodo.storage.model.ThingToDo
+import kotlinx.coroutines.launch
+import java.util.*
 
-class ThingToDoViewModel(private val thingToDoDao: ThingToDoDao) :ViewModel() {}
+class ThingToDoViewModel(private val thingToDoDao: ThingToDoDao) :ViewModel() {
+
+    private fun getNewThingToDoEntry(thingToDoName : String, thingToDoDescription: String, thingToDoDate: Date): ThingToDo{
+        return ThingToDo(
+            name = thingToDoName,
+            description = thingToDoDescription,
+            timeStamp = thingToDoDate
+        )
+    }
+
+    private fun insertThingToDo(thingToDo: ThingToDo){
+        viewModelScope.launch {
+            thingToDoDao.insertThingToDo(thingToDo)
+        }
+    }
+
+    fun addNewThingToDo(thingToDoName : String, thingToDoDescription: String, thingToDoDate: Date){
+
+        val newThingToDo = getNewThingToDoEntry(thingToDoName, thingToDoDescription, thingToDoDate)
+        insertThingToDo(newThingToDo)
+
+    }
+}
 
 class ThingToDoViewModelFactor(private val thingToDoDao: ThingToDoDao) : ViewModelProvider.Factory {
 
