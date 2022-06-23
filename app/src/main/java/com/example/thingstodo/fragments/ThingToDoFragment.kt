240789fae23bc36efafee484cdc0a53309a380b7
RyanKoech <sirryankoech@gmail.com>
2022-06-23 08:30:35 +0300

    Added Logic to inflate Layout

--- app/src/main/java/com/example/thingstodo/fragments/ThingToDoFragment.kt
+++ app/src/main/java/com/example/thingstodo/fragments/ThingToDoFragment.kt
@@ -5,6 +5,51 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import com.example.thingstodo.R
+import com.example.thingstodo.application.ThingToDoApplication
+import com.example.thingstodo.databinding.FragmentThingToDoBinding
+import com.example.thingstodo.viewmodel.ThingToDoViewModel
+import com.example.thingstodo.viewmodel.ThingToDoViewModelFactor
+import kotlin.properties.Delegates
 
-class ThingToDoFragment : Fragment()
+class ThingToDoFragment : Fragment() {
+
+    companion object {
+        var ID = "id"
+    }
+
+    private var _binding : FragmentThingToDoBinding? = null
+    private val binding get() = _binding!!
+    private var thingToDoId by Delegates.notNull<Int>()
+
+    private val viewModel : ThingToDoViewModel by activityViewModels{
+        ThingToDoViewModelFactor(
+            (activity?.application as ThingToDoApplication).database
+                .thingToDoDao()
+        )
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+
+        arguments?.let{
+            thingToDoId = it.getInt(ID)
+        }
+    }
+
+    override fun onCreateView(inflater : LayoutInflater, container: ViewGroup?, savedInstaceState: Bundle? ): View?{
+
+        _binding = FragmentThingToDoBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        return view
+    }
+
+    override fun onDestroy(){
+        super.onDestroy()
+        _binding = null
+    }
+
+}
