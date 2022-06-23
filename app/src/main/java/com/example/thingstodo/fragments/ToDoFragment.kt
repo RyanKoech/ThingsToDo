0e8f041eeee272d5f30f5a7779c2be09d5d14865
RyanKoech <sirryankoech@gmail.com>
2022-06-23 11:45:35 +0300

    Implemented options menu

--- app/src/main/java/com/example/thingstodo/fragments/ToDoFragment.kt
+++ app/src/main/java/com/example/thingstodo/fragments/ToDoFragment.kt
@@ -1,9 +1,7 @@
 package com.example.thingstodo.fragments
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.findNavController
@@ -68,4 +66,7 @@ class ToDoFragment : Fragment() {
         _binding = null
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater){
+        inflater.inflate(R.menu.main_menu, menu)
+    }
 }
