633787ecbcbe61cce19b8127a533afdade33e1e3
RyanKoech <sirryankoech@gmail.com>
2022-06-25 20:33:12 +0300

    Implemented checked change listener to toggle modes

--- app/src/main/java/com/example/thingstodo/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/thingstodo/fragments/SettingsFragment.kt
@@ -1,16 +1,21 @@
 package com.example.thingstodo.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.thingstodo.MainActivity
+import com.example.thingstodo.SharedPref
 import com.example.thingstodo.databinding.FragmentSettingsBinding
 
 class SettingsFragment : Fragment() {
 
     private var _binding : FragmentSettingsBinding? = null
     private val binding get() = _binding!!
+    internal lateinit var sharedPref: SharedPref
+
 
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
@@ -21,12 +26,30 @@ class SettingsFragment : Fragment() {
         _binding = FragmentSettingsBinding.inflate(inflater, container, false)
         val view = binding.root
 
+        sharedPref = (activity as MainActivity).sharedPref
+
+        binding.toggleDarkmodeSwitch.apply {
+            isChecked = sharedPref.loadNightModeState()!!
+            setOnCheckedChangeListener{ buttonView, isChecked ->
+
+                sharedPref.setNightModeState(isChecked)
+                restartApp()
+
+            }
+        }
+
         return view
     }
 
     override fun onDestroy(){
         super.onDestroy()
         _binding = null
+    }
+
+    private fun restartApp() {
+        val intent = Intent ((activity as MainActivity).applicationContext, MainActivity::class.java)
+        startActivity(intent)
+        (activity as MainActivity).finish()
     }
 
 }
