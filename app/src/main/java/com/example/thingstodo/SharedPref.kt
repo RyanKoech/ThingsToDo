010a416700c7d3f4d0fbec5d57736bf4354fa0b6
RyanKoech <sirryankoech@gmail.com>
2022-06-25 20:32:01 +0300

    Added Class to store user preferences

---
+++ app/src/main/java/com/example/thingstodo/SharedPref.kt
@@ -0,0 +1,25 @@
+package com.example.thingstodo
+
+import android.content.Context
+import android.content.SharedPreferences
+
+class SharedPref(context: Context) {
+
+    internal lateinit var sharedPref: SharedPreferences
+
+    init {
+        sharedPref = context.getSharedPreferences("filename", Context.MODE_PRIVATE)
+    }
+
+    fun setNightModeState(state: Boolean?){
+        val editor : SharedPreferences.Editor = sharedPref.edit()
+        editor.putBoolean("Night Mode", state!!)
+        editor.commit()
+    }
+
+    fun loadNightModeState() : Boolean? {
+        return sharedPref.getBoolean("Night Mode", false)
+    }
+
+
+}
