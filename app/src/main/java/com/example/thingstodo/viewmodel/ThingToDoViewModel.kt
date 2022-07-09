477830ae944f74c05e5cc66f5348245366b5776b
RyanKoech <sirryankoech@gmail.com>
2022-07-09 17:06:06 +0300

    Implemented basic scheduling of a task using alarmManager

--- app/src/main/java/com/example/thingstodo/viewmodel/ThingToDoViewModel.kt
+++ app/src/main/java/com/example/thingstodo/viewmodel/ThingToDoViewModel.kt
@@ -1,11 +1,15 @@
 package com.example.thingstodo.viewmodel
 
 import android.app.AlarmManager
+import android.app.PendingIntent
 import android.content.Context.ALARM_SERVICE
+import android.content.Intent
+import android.widget.Toast
 import androidx.lifecycle.*
 import com.example.thingstodo.MainActivity
 import com.example.thingstodo.storage.dao.ThingToDoDao
 import com.example.thingstodo.model.ThingToDo
+import com.example.thingstodo.receiver.ThingToDoReceiver
 import kotlinx.coroutines.launch
 import java.util.*
 
@@ -43,10 +47,26 @@ class ThingToDoViewModel(private val thingToDoDao: ThingToDoDao, private val act
         }
     }
 
+    private fun scheduleReminder() {
+        val intent = Intent(activity.applicationContext, ThingToDoReceiver::class.java)
+        val pendingIntent = PendingIntent.getBroadcast(activity.applicationContext, 0, intent, 0)
+
+        val calendar = Calendar.getInstance()
+        println(calendar)
+        alarmManager.set(
+            AlarmManager.RTC,
+            calendar.timeInMillis + 60,
+            pendingIntent
+        )
+
+        Toast.makeText(activity.applicationContext, "Toast Scheduled successfully", Toast.LENGTH_SHORT).show()
+    }
+
     fun addNewThingToDo(thingToDoName : String, thingToDoDescription: String, thingToDoDate: Date){
 
         val newThingToDo = getNewThingToDoEntry(thingToDoName, thingToDoDescription, thingToDoDate)
         insertThingToDo(newThingToDo)
+        scheduleReminder()
 
     }
 
