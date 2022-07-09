db56e7b5d672eb4d23e7808c2167f84b6768c209
RyanKoech <sirryankoech@gmail.com>
2022-07-09 16:59:07 +0300

    Implemented receiver onReceive function

--- app/src/main/java/com/example/thingstodo/receiver/ThingToDoReceiver.kt
+++ app/src/main/java/com/example/thingstodo/receiver/ThingToDoReceiver.kt
@@ -1,3 +1,12 @@
 package com.example.thingstodo.receiver
 
-class ThingToDoReceiver
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+class ThingToDoReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        Toast.makeText(context!!, "Task due in about 30 min", Toast.LENGTH_LONG).show()
+    }
+}
