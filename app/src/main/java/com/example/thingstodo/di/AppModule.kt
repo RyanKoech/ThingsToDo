74efcba0df1c0fb5e419bc9024f1baf55a97b84d
RyanKoech <sirryankoech@gmail.com>
2022-08-04 17:14:45 +0300

    Added Module file and defined creation of some dependencies

---
+++ app/src/main/java/com/example/thingstodo/di/AppModule.kt
@@ -0,0 +1,41 @@
+package com.example.thingstodo.di
+
+import android.content.Context
+import androidx.room.Room
+import com.example.thingstodo.other.Constants.DATABASE_NAME
+import com.example.thingstodo.storage.database.ThingToDoRoomDatabase
+import com.example.thingstodo.utilities.ContextProvider
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideThingDoToDatabase(
+        @ApplicationContext context: Context
+    ) = Room.databaseBuilder(context, ThingToDoRoomDatabase::class.java, DATABASE_NAME).build()
+
+    @Provides
+    @Singleton
+    fun provideThingToDoDao(
+        database: ThingToDoRoomDatabase
+    ) = database.thingToDoDao()
+
+    @Provides
+    @Singleton
+    fun provideContextProvider(@ApplicationContext context: Context) : ContextProvider {
+        return object : ContextProvider {
+            override fun getContext(): Context {
+                return context
+            }
+
+        }
+    }
+}
