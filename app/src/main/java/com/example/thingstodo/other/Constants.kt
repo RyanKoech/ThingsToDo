7aec2bc581e99c5c0a87e5353fcb00fbfd9369d0
RyanKoech <sirryankoech@gmail.com>
2022-08-05 16:15:04 +0300

    Added constants

--- app/src/main/java/com/example/thingstodo/other/Constants.kt
+++ app/src/main/java/com/example/thingstodo/other/Constants.kt
@@ -3,5 +3,7 @@ package com.example.thingstodo.other
 object Constants {
 
     const val DATABASE_NAME  = "thing_to_do_database"
+    const val MILLISECONDS_30MIN = 1800000
+    const val TAG_TASK_NAME = "TASK_NAME"
 
 }
