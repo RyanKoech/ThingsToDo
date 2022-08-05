19b439f9b70f09ade3a30efe121fa77feb7143e0
RyanKoech <sirryankoech@gmail.com>
2022-08-05 16:17:14 +0300

    Fetching varaible from intent

--- app/src/main/java/com/example/thingstodo/receiver/ThingToDoReceiver.kt
+++ app/src/main/java/com/example/thingstodo/receiver/ThingToDoReceiver.kt
@@ -4,9 +4,11 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.widget.Toast
+import com.example.thingstodo.other.Constants
 
 class ThingToDoReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
+        val thingToDoName : String = intent?.getStringExtra(Constants.TAG_TASK_NAME).toString()
         Toast.makeText(context!!, "Task due in about 30 min", Toast.LENGTH_LONG).show()
     }
 }
