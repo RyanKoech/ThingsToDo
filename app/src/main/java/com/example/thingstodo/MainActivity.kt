f4d1b7f04e2862437c00c69cd00c6c043db4f130
RyanKoech <sirryankoech@gmail.com>
2022-08-09 13:47:04 +0300

    Implemented conditional  autonavigation to ThingToDo fragment

--- app/src/main/java/com/example/thingstodo/MainActivity.kt
+++ app/src/main/java/com/example/thingstodo/MainActivity.kt
@@ -5,6 +5,9 @@ import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.thingstodo.fragments.ThingToDoFragment
+import com.example.thingstodo.fragments.ToDoFragmentDirections
+import com.example.thingstodo.other.Constants
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -17,14 +20,27 @@ class MainActivity : AppCompatActivity() {
         // AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_YES)
         // setTheme(R.style.Theme_ThingsToDo_Dark)
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
+
+        val initialFragment = intent.getStringExtra(Constants.INITIAL_FRAGMENT)
+        println("This fragment " + initialFragment)
+        println(intent.extras)
 
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.main_fragment) as NavHostFragment
         navController = navHostFragment.navController
 
         setupActionBarWithNavController(navController)
+
+        initialFragment?.let {
+            if(it == ThingToDoFragment.TAG){
+                val thingToDoid = this@MainActivity.intent.getIntExtra(ThingToDoFragment.ID, -1)
+                if(thingToDoid >= 0) {
+                    val action = ToDoFragmentDirections.actionToDoFragmentToThingToDoFragment(id = thingToDoid)
+                    navController.navigate(action)
+                }
+            }
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
