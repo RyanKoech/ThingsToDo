faeeca34ee039f44546343ca2f6198b1cd36bc6a
RyanKoech <sirryankoech@gmail.com>
2022-08-09 13:43:40 +0300

    Created notification channel

--- app/src/main/java/com/example/thingstodo/application/ThingToDoApplication.kt
+++ app/src/main/java/com/example/thingstodo/application/ThingToDoApplication.kt
@@ -1,7 +1,32 @@
 package com.example.thingstodo.application
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.graphics.Color
+import android.os.Build
+import com.example.thingstodo.other.Constants
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class ThingToDoApplication : Application() {}
+class ThingToDoApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        createNotificationChannel()
+    }
+
+    private fun createNotificationChannel() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(Constants.NOTIFICATIONS_CHANNEL_ID, Constants.NOTIFICATIONS_CHANNEL_NAME, NotificationManager.IMPORTANCE_HIGH)
+                .apply {
+                    lightColor = Color.GREEN
+                    enableLights(true)
+                    description = "Used to show tasks due in 30 minutes or less"
+                }
+            val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            manager.createNotificationChannel(channel)
+        }
+    }
+}
