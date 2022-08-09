b7dca34112c71adb1f86a129b2960b739bf27407
RyanKoech <sirryankoech@gmail.com>
2022-08-09 13:43:16 +0300

    Added new constants

--- app/src/main/java/com/example/thingstodo/other/Constants.kt
+++ app/src/main/java/com/example/thingstodo/other/Constants.kt
@@ -5,5 +5,9 @@ object Constants {
     const val DATABASE_NAME  = "thing_to_do_database"
     const val MILLISECONDS_30MIN = 1800000
     const val TAG_TASK_NAME = "TASK_NAME"
+    const val TAG_TASK_ID = "TASK_ID"
+    const val NOTIFICATIONS_CHANNEL_ID = "notifications_id"
+    const val NOTIFICATIONS_CHANNEL_NAME = "Task Due"
+    const val INITIAL_FRAGMENT = "INITIAL_FRAGMENT"
 
 }
