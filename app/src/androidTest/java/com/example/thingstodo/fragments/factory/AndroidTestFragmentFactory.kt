e431a8cdaa4fbbd6f78ff9b2de9dc886da7b4002
RyanKoech <sirryankoech@gmail.com>
2022-08-23 12:16:44 +0300

    Defined ThingToDo fragment factory implementation

--- app/src/androidTest/java/com/example/thingstodo/fragments/factory/AndroidTestFragmentFactory.kt
+++ app/src/androidTest/java/com/example/thingstodo/fragments/factory/AndroidTestFragmentFactory.kt
@@ -6,7 +6,9 @@ import androidx.fragment.app.FragmentFactory
 import androidx.test.platform.app.InstrumentationRegistry
 import com.example.thingstodo.fragments.ThingToDoFragment
 import com.example.thingstodo.fragments.ToDoFragment
+import com.example.thingstodo.model.ThingToDo
 import com.example.thingstodo.repository.FakeThingToDoRepository
+import com.example.thingstodo.utilities.AndroidTestUtilities
 import com.example.thingstodo.utilities.ContextProvider
 import com.example.thingstodo.viewmodel.ThingToDoViewModel
 
@@ -23,6 +25,19 @@ class AndroidTestFragmentFactory : FragmentFactory() {
                 val repository = FakeThingToDoRepository()
                 val viewModel = ThingToDoViewModel(repository, contextProvider)
                 ToDoFragment(viewModel)
+            }
+            ThingToDoFragment::class.java.name -> {
+                val initialThingsToDo = listOf<ThingToDo>(
+                    AndroidTestUtilities.getValidThingToDo(0),
+                )
+                val contextProvider = object : ContextProvider{
+                    override fun getContext(): Context {
+                        return InstrumentationRegistry.getInstrumentation().targetContext
+                    }
+                }
+                val repository = FakeThingToDoRepository(initialThingsToDo)
+                val viewModel = ThingToDoViewModel(repository, contextProvider)
+                ThingToDoFragment(viewModel)
             }
             else -> super.instantiate(classLoader, className)
         }
