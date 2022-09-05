f7f56039bcbf97cfd7685f63dfa6b20e87f3a530
RyanKoech <sirryankoech@gmail.com>
2022-09-05 14:17:31 +0300

    Implemented back navigation to ThingToDoFragment

--- app/src/androidTest/java/com/example/thingstodo/fragments/ThingToDoFragmentTest.kt
+++ app/src/androidTest/java/com/example/thingstodo/fragments/ThingToDoFragmentTest.kt
@@ -9,12 +9,15 @@ import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.testing.TestNavHostController
 import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.*
 import com.example.thingstodo.R
 import com.example.thingstodo.fragments.factory.AndroidTestFragmentFactory
 import com.example.thingstodo.utilities.AndroidTestUtilities
+import com.google.common.truth.Truth
 import org.hamcrest.Matchers
 import com.google.common.truth.Truth.assertThat
 import org.junit.Before
@@ -135,4 +138,34 @@ class ThingToDoFragmentTest {
         assertThat(navController.currentDestination?.id).isEqualTo(R.id.editToDoFragment)
 
     }
+
+    @Test
+    fun pressBackButton_navigateToThingToToFragment() {
+
+        val navController = TestNavHostController(
+            ApplicationProvider.getApplicationContext()
+        )
+
+        val scenario = launchFragmentInContainer<ThingToDoFragment>(
+            factory = androidTestFragmentFactory,
+            themeResId = R.style.Theme_ThingsToDo,
+            initialState = Lifecycle.State.CREATED,
+            fragmentArgs = bundleOf(EditToDoFragment.ID to 0)
+        )
+
+        scenario.moveToState(Lifecycle.State.RESUMED)
+
+        scenario.onFragment{
+            navController.setGraph(R.navigation.nav_graph)
+            navController.setCurrentDestination(R.id.thingToDoFragment)
+            Navigation.setViewNavController(it.requireView(), navController)
+        }
+
+        onView(withId(R.id.edit_thing_to_do_fab)).perform(click())
+
+        navController.navigateUp()
+        assertThat(navController.currentDestination?.id).isEqualTo(R.id.thingToDoFragment)
+
+    }
+
 }
